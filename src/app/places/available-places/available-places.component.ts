d01423cf8076317d7da0966588de6526563e5317
Mark Paul <markpaulbusiness66@gmail.com>
2025-05-19 15:14:22 +0300

    feat: integrate HTTP client and implement data fetching in AvailablePlacesComponent

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component,DestroyRef,OnInit, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
@@ -13,4 +14,22 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+  
+  ngOnInit(){
+    const subscription = this.httpClient.get<{places: Place[]  }>("http://localhost:3000/places", {
+      observe: 'response'
+    }).pipe().subscribe({
+      next: (response)=> {
+        this.places.set(response.body?.places)
+      }
+    })
+
+    this.destroyRef.onDestroy(()=>{
+      subscription.unsubscribe()
+    })
+
+    
+  }
 }
